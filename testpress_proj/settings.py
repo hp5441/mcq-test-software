7b8717c61c9ca2aa9d79d75885454363ca684f83
hari <hram5441@gmail.com>
2021-05-31 16:29:39 +0530

    changed folder structure

--- testpress_proj/settings.py
+++ testpress_proj/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
-STATICFILES_DIRS = STATIC_ROOT
+STATICFILES_DIRS = [STATIC_ROOT, ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
